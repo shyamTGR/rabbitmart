97698b5042b28ec7640031cc8d4c03a61a2fa392
Shyam Sunder <syamsunderthambabathula@gmail.com>
2023-11-25 18:19:01 -0600

    Update BaseURLs.js

--- products/services/BaseURLs.js
+++ products/services/BaseURLs.js
@@ -1,2 +1,2 @@
-export const PRODUCTS_BASEURL = 'https://rabbitmart-products.vercel.app/products'
-export const USER_BASEURL = 'https://rabbitmart-users.vercel.app/me'
+export const PRODUCTS_BASEURL = 'https://rabbitmart-ell1-887hg4751-shyams-projects-78ac3144.vercel.app/products'
+export const USER_BASEURL = 'https://rabbitmart-ell1-887hg4751-shyams-projects-78ac3144.vercel.app/me'
